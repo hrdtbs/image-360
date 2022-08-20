c7857b16aa1029b4b37a23d28083936f216a8edc
hrdtbs <harada.tsubasa.0422@gmail.com>
2022-08-20 23:49:09 +0900

    🐛 use vite base instead of base element

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,5 @@
 import { defineConfig } from "vite";
 
-import { createHtmlPlugin } from "vite-plugin-html";
-
 export default defineConfig({
-  plugins: [
-    createHtmlPlugin({
-      minify: true,
-      template: "index.html",
-      inject: {
-        data: {
-          BASE_URL: process.env.BASE_URL || "/",
-        },
-      },
-    }),
-  ],
+  base: "/theta-360/",
 });
